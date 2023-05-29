771bc39c534d00aef1dd18c74b0d77b0abac738c
danielmegale <danim1@live.it>
2023-05-29 17:12:46 +0200

    added button

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
 console.log('JS OK')
 KmPrice = 0.21 
 //Prendiamo un elemnto dal DOM
-const element =document.getElementById('ticket');
-const couponTicket =document.getElementById('coupon-ticket');
-const user =document.getElementById('user-age')
+let  element =document.getElementById('ticket');
+let couponTicket =document.getElementById('coupon-ticket');
+const Age =document.getElementById('user-age')
 const Km =document.getElementById('user-km')
+const Button =document.getElementById('button')
+const Name =document.getElementById('user')
+const userTicket=document.getElementById('user-ticket')
 
-//User Info
-const userAge =parseInt(prompt('Inserisci la tua età', 45));
-console.log(userAge + ' ' + 'Anni', typeof userAge);
+// al click del button
+Button.addEventListener('click', function(){
+    //User Info
+    const user = Name.value.trim();
+    console.log(user)
+    
+    const userAge = Age.value.trim();
+    console.log(userAge);
+    // + ' ' + 'Anni', typeof userAge
+    const userKm =parseInt( Km.value.trim());
+    console.log(userKm +'Km', typeof userKm);
+    userTicket.innerText ='Nome passeggero: ' + user;
 
-
-const userKm =parseInt(prompt('Inerisci i kilometri che vuoi percorrere',100));
-console.log(userKm+'Km', typeof userKm);
-
-//Validatore
-let coupon= 1;
-let newPrice;
-if (isNaN(userAge) || isNaN(userKm)){
-    alert('Valore non valido')
-}else if ((userAge < 0) || (userKm < 0)){
-    alert('Valore negativo non valido')
-}else{
-    const ticketPrice = (userKm * KmPrice).toFixed(2);
-    console.log(ticketPrice);
-    element.innerText +=' '+ ticketPrice +'€';
-    user.innerText +=' ' + userAge + 'Anni';
-    Km.innerText +=' '+ userKm + 'Km';
-
-    if (userAge < 18 ){
-        coupon = 0.80;
-        console.log(coupon);
-        newPrice = ticketPrice * coupon;
-        console.log(newPrice);
-        couponTicket.innerText +='Biglietto scontato' + ' ' +  (newPrice).toFixed(2) + '€';
-    }else if (userAge >=65){
-        coupon = 0.60;
-        console.log(coupon);
-        newPrice = ticketPrice * coupon;
-        console.log(newPrice);
-        couponTicket.innerText +='Biglietto scontato' + ' ' +  (newPrice).toFixed(2) + '€';
+    //Validatore
+    let coupon= 1;
+    let newPrice;
+    if (isNaN(userKm)){
+        alert('Valore non valido')
+    }else if ((userAge<=0) || (userKm <=0)){
+        alert('Valore negativo non valido')
+    }else{
+        const ticketPrice = (userKm * KmPrice).toFixed(2);
+        console.log(ticketPrice);
+        element.innerText ='Prezzo biglietto: '+ ticketPrice +'€';
+        couponTicket.innerText='Offerta: ' + 'Biglietto standard';
+        
+        if (userAge ==='minorenne'){
+             coupon = 0.80;
+             console.log(coupon);
+             newPrice = ticketPrice * coupon;
+            console.log(newPrice);
+            element.innerText ='Prezzo Biglietto' + ' ' +  (newPrice).toFixed(2) + '€';
+            couponTicket.innerText='Offerta: ' + 'Biglietto Ridotto';
+        }else if (userAge === 'over65'){
+            coupon = 0.60;
+            console.log(coupon);
+            newPrice = ticketPrice * coupon;
+             console.log(newPrice);
+            element.innerText ='Prezzo Biglietto' + ' ' +  (newPrice).toFixed(2) + '€';
+            couponTicket.innerText='Offerta: ' + 'Biglietto Senior';
+        }
     }
-}
-
+});
